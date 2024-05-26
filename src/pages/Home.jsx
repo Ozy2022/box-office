3e9517b3e6e8af8254a19f49596e07fc148755b6
Osama Ahemd <ozy.almondry2021@gmail.com>
2024-05-26 08:37:59 +0300

    added texbox for searching, search for shows on button click

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [SearchStr, setSearchStr] = useState('');
+
+  const onInputChange = ev => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async ev => {
+    ev.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q= ${SearchStr}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={SearchStr} onChange={onInputChange} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
