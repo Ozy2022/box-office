41c1d7a380419d6ab66e0e2f03915957918d428f
Osama Ahemd <ozy.almondry2021@gmail.com>
2024-05-26 11:11:28 +0300

    Fetch and displaying data from tvmaze api, create a reusble function to fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,48 @@
 import { useState } from 'react';
-
+import { searchForShows } from '../api/tvmaze';
 const Home = () => {
   const [SearchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState(null);
+  const [apiDataError, setApiDataError] = useState(null);
 
-  const onInputChange = ev => {
+  const onSearchInputChane = ev => {
     setSearchStr(ev.target.value);
   };
 
   const onSearch = async ev => {
     ev.preventDefault();
 
-    const response = await fetch(
-      `https://api.tvmaze.com/search/shows?q= ${SearchStr}`
-    );
-    const body = await response.json();
-    console.log(body);
+    try {
+      setApiDataError(null);
+
+      const results = await searchForShows(SearchStr);
+      setApiData(results);
+    } catch (error) {
+      setApiDataError(error);
+    }
   };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error occuerd: {apiDataError.message}</div>;
+    }
+
+    if (apiData) {
+      return apiData.map(data => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    }
+    return null;
+  };
+
   return (
     <div>
       <form onSubmit={onSearch}>
-        <input type="text" value={SearchStr} onChange={onInputChange} />
+        <input type="text" value={SearchStr} onChange={onSearchInputChane} />
         <button type="submit">Search</button>
       </form>
+
+      <div>{renderApiData()}</div>
     </div>
   );
 };
