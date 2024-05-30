0f7b274f93b3e42ad9fb7f925b98e4cad3fea85d
Osama Ahemd <ozy.almondry2021@gmail.com>
2024-05-30 14:08:26 +0300

    displayed detailed show data on the show page

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,40 +1,17 @@
 import { useParams } from 'react-router-dom';
 import { getShowById } from '../api/tvmaze';
 import { useQuery } from '@tanstack/react-query';
-
-//#1 fetch show data solution by useEffect() logic
-
-// const useShowById = showId => {
-//   const [showData, setShowData] = useState(null);
-//   const [showError, setShowError] = useState(null);
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const data = await getShowById(showId);
-//         setShowData(data);
-//       } catch (err) {
-//         setShowError(err);
-//       }
-//     }
-//     fetchData();
-//   }, [showId]);
-
-//   return [showData, showError];
-// };
-
+import Details from '../components/shows/Details';
+import ShowMainData from '../components/shows/ShowMainData';
+import Seasons from '../components/shows/Seasons';
+import Cast from '../components/shows/Cast';
 const Show = () => {
   const { showId } = useParams();
-  //  1- S => const { showData, showError } = useShowById(showId);
-
-  //#2 fetch show data solution by useQuery() hook
-  //and that would avoide the repetition logic
-  //and the <React.StrictMode> fetch data
-  //issue which would fetch the data only ones
 
   const { data: showData, error: showError } = useQuery({
     queryKey: ['show', showId],
     queryFn: () => getShowById(showId),
+    refetchOnWindowFocus: false,
   });
 
   if (showError) {
@@ -42,7 +19,35 @@ const Show = () => {
   }
 
   if (showData) {
-    return <div>Got show data: {showData.name}</div>;
+    return (
+      <div>
+        <ShowMainData
+          image={showData.image}
+          name={showData.name}
+          rating={showData.rating}
+          summary={showData.summary}
+          genres={showData.genres}
+        />
+
+        <div>
+          <h2>Details</h2>
+          <Details
+            status={showData.status}
+            premiered={showData.premiered}
+            network={showData.network}
+          />
+        </div>
+
+        <div>
+          <h2>Seasons</h2>
+          <Seasons seasons={showData._embedded.seasons} />
+        </div>
+        <div>
+          <h2>Cast</h2>
+          <Cast cast={showData._embedded.cast}/>
+        </div>
+      </div>
+    );
   }
 
   return <div>Data is Loading</div>;
